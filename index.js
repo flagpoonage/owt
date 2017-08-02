be3e8b8fc6ef25002a356e1b2b61c5871424aa6f
James Hay <pokinoice@live.com>
2017-08-02 20:10:25 +0800

    Update to fix something possibly

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const _ = (fn, tcolor, bcolor, ...args) => {
     return;
   }
 
-  fn(chalk.rgb(210, 214, 216)(moment().format('HH:mm:ss DD/MM/YYYY Z') + ' -- ') + tcolor(args[0]) + chalk.rgb(210, 214, 216)(' -- '));
+  fn(chalk.rgb(210, 214, 216)(moment().format('HH:mm:ss DD/MM/YYYY Z') + ' -- ') + tcolor()(args[0]) + chalk.rgb(210, 214, 216)(' -- '));
 
   args.slice(1).map(a => {
     if (a instanceof Error) {
-      return fn(bcolor(a.stack));
+      return fn(bcolor()(a.stack));
     }
 
     let output = OWTJ(a, null, 2);
-    fn(bcolor(output ? output.replace(/"(\w+)"\s*:/g, '$1:') : null));
+    fn(bcolor()(output ? output.replace(/"(\w+)"\s*:/g, '$1:') : null));
   });
 };
 
@@ -27,11 +27,11 @@ var output = {
     const CS = new Console(process.stdout, process.stderr);
 
     global.console.log = (...args) => {
-      _(CS.log, chalk.rgb(50, 209, 50), chalk.rgb(71, 197, 255), ...args);
+      _(CS.log, () => chalk.rgb(50, 209, 50), () => chalk.rgb(71, 197, 255), ...args);
     },
 
     global.console.error = (...args) => {
-      _(CS.error, chalk.rgb(209, 50, 50), chalk.rgb(255, 197,71), ...args);
+      _(CS.error, () => chalk.rgb(209, 50, 50), () => chalk.rgb(255, 197,71), ...args);
     };
   },
 
